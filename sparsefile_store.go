73334996c34543ae9f21b798df08c357dd4ee02a
Aaron Marburg <amarburg@apl.washington.edu>
2016-12-21 16:13:42 -0800

    Implementing basic Sparse update functions.

--- sparsefile_store.go
+++ sparsefile_store.go
@@ -16,10 +16,11 @@ func (e SparseFileStoreError) Error() string {
 //=====================================
 type SparseFileStore struct {
 	file *os.File
+	has map[int64]bool
 }
 
 func OpenSparseFileStore( name string ) (*SparseFileStore,error) {
-	f,err := os.Open(name)
+	f,err := os.OpenFile(name, os.O_RDWR, 0644 )
 	fs := SparseFileStore{ file: f }
 
 	return &fs, err
@@ -27,24 +28,40 @@ func OpenSparseFileStore( name string ) (*SparseFileStore,error) {
 
 //=====================================
 func (fs *SparseFileStore) ReadAt( p []byte, off int64) (n int, err error) {
+	// Check validity
+	n,err =  fs.HasAt( p, off )
+	if err != nil {
+		return 0, SparseFileStoreError{"Don't have all of the requested bytes"}
+	}
+
 	return fs.file.ReadAt( p, off )
 }
 
 func (fs *SparseFileStore) WriteAt(p []byte, off int64) (n int, err error) {
-	return 0,nil
+	n,err =  fs.file.WriteAt( p, off )
+
+	for i:=0; i < n; i++ {
+		fs.has[off + int64(i)] = true;
+	}
+
+	return n, err
 }
 
 func (fs *SparseFileStore) HasAt( p []byte, off int64 ) (n int, err error) {
-	len := int64(cap( p ))
-	sz,_ := fs.FileSize()
-
-	switch {
-		case (off + len) < sz: return int(len), nil
-		case off > sz: return 0, SparseFileStoreError{"Offset beyond end of file"}
-		case (off + len) > sz: return int(sz - off), nil
+	sz,err := fs.FileSize();
+	if off+int64(cap(p)) > sz {
+		n = int(sz - off)
+	} else {
+		n = cap(p)
 	}
 
-	return 0, SparseFileStoreError{"Shouldn't get here"}
+	for i:= 0; i < n; i++ {
+		if fs.has[off+int64(i)] == false {
+			return 0, SparseFileStoreError{"Don't have all of the bytes requested"}
+		}
+	}
+
+	return n, nil
 }
 
 func (fs *SparseFileStore) FileSize() (int64,error) {
--- sparsefile_store_test.go
+++ sparsefile_store_test.go
@@ -13,4 +13,18 @@ func TestSparseFileStore(t *testing.T) {
     t.Fatal("Error opening SparseFileStore", err.Error() )
   }
 
+  // Should fail
+  buf := make([]byte,10)
+  n,err := fs.HasAt( buf, 0 )
+
+  if n != 0 || err == nil {
+    t.Error("HasAt should have failed, but didn't")
+  }
+
+  n,err = fs.ReadAt( buf, 0 )
+
+  if n != 0 || err == nil {
+    t.Error("ReadAt should have failed, but didn't")
+  }
+
 }
