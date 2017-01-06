bbf046e09440ba84a4aae29494a44a57a0a63ff9
Aaron Marburg <amarburg@apl.washington.edu>
2017-01-06 10:17:08 -0800

    Reducing the lazyfs interfaces to a minimal level.

--- lazyfile.go
+++ lazyfile.go
@@ -3,8 +3,8 @@ package lazyfs
 import "io"
 
 type File interface {
-io.ReaderAt
-FileSize() (int64,error)
+  io.ReaderAt
+  FileSize() (int64,error)
 }
 
 type FileSource interface {
@@ -14,30 +14,6 @@ type FileSource interface {
 }
 
 type FileStorage interface {
-    File
-    //io.WriterAt
-  HasAt( p []byte, off int64 ) (int, error)
+  File
+  //HasAt( p []byte, off int64 ) (int, error)
 }
-
-// type LazyFile struct {
-//   storage FileStorage
-//   source FileSource
-// }
-//
-// func (fs *LazyFile) ReadAt( p []byte, off int64 ) (n int, err error) {
-//   n,err = fs.storage.HasAt( p, off )
-//
-// 	if err == nil {
-//     return fs.storage.ReadAt( p, off )
-//   } else {
-//     if fs.source == nil { return 0, LazyFSError { "Source not defined" } }
-//
-//     n,err = fs.source.ReadAt( p, off )
-//
-//     if cap(p) != n { return n, LazyFSError { "Short read!"}}
-//
-//     _,err = fs.storage.WriteAt( p, off )
-//
-//     return n,err
-//   }
-// }
