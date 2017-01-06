cab1c517113260a1c5254115ce08e2b3621268ae
Aaron Marburg <amarburg@apl.washington.edu>
2017-01-06 10:19:29 -0800

    Drop down a single interface (Store can be a Source?)

--- lazyfile.go
+++ lazyfile.go
@@ -2,18 +2,9 @@ package lazyfs
 
 import "io"
 
-type File interface {
+type FileSource interface {
   io.ReaderAt
   FileSize() (int64,error)
-}
-
-type FileSource interface {
-  File
   Reader() (io.Reader)
   Path() string
-}
-
-type FileStorage interface {
-  File
-  //HasAt( p []byte, off int64 ) (int, error)
 }
