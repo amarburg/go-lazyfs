c6be6f89d3608fa2e93d9dcef81aca3235b58cca
Aaron Marburg <amarburg@apl.washington.edu>
2017-09-22 07:57:46 -0800

    First rough read-only version of BlockStore.

---
+++ block_store_test.go
@@ -0,0 +1,82 @@
+package lazyfs
+
+import "testing"
+//import "net/url"
+import "io"
+import "github.com/amarburg/go-lazyfs-testfiles"
+//import "github.com/amarburg/go-lazyfs-testfiles/http_server"
+
+func TestBlockStore(t *testing.T) {
+	src, _ := OpenLocalFileSource(LocalFilesRoot, lazyfs_testfiles.AlphabetFile)
+	fs, err := OpenBlockStore(src,8)
+
+	if fs == nil {
+		t.Fatal("BlockStore doesn't exist")
+	}
+
+	if err != nil {
+		t.Fatal("Error opening BlockStore", err.Error())
+	}
+
+	for _, test := range test_pairs {
+
+		buf := make([]byte, 0, BufSize)
+
+		// Test ReadAt
+		n, err := fs.ReadAt(buf, test.offset)
+
+		if err != nil && err != io.EOF {
+			t.Errorf("Error on read: %s", err.Error())
+		}
+
+		if n != test.length {
+			t.Error("Expected", test.length, "bytes, got", n)
+		}
+
+		buf = buf[:n]
+
+		if !CheckTestFile(buf, test.offset) {
+			t.Errorf("\"%s\" doesn't match test file at %d", n, 0)
+		}
+
+	}
+
+}
+//
+// func TestHttpSourceSparseStore(t *testing.T) {
+//
+// 	srv := lazyfs_testfiles_http_server.HttpServer()
+//
+// 	AlphabetUrl, _ := url.Parse(srv.Url + lazyfs_testfiles.AlphabetFile)
+// 	source, err := OpenHttpSource(*AlphabetUrl)
+// 	if err != nil {
+// 		t.Fatal("Couldn't open HttpFSSource")
+// 	}
+//
+// 	store, err := OpenSparseFileStore(source, HttpSourceSparseStore)
+//
+// 	for _, test := range test_pairs {
+//
+// 		buf := make([]byte, BufSize)
+//
+// 		// Test ReadAt
+// 		n, err := store.ReadAt(buf, test.offset)
+//
+// 		if err != nil && err != io.EOF {
+// 			t.Errorf("Error on read: %s", err.Error())
+// 		}
+//
+// 		if n != test.length {
+// 			t.Error("Expected", test.length, "bytes, got", n)
+// 		}
+//
+// 		buf = buf[:n]
+//
+// 		if !CheckTestFile(buf, test.offset) {
+// 			t.Errorf("\"%s\" doesn't match test file at %d", buf, test.offset)
+// 		}
+//
+// 	}
+//
+// 	srv.Stop()
+// }
