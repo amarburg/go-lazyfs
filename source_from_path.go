34d1332c907301be8e5a29f908089536c258c441
Aaron Marburg <amarburg@apl.washington.edu>
2017-12-14 22:19:43 -0800

    Added source_from_path.go

---
+++ source_from_path.go
@@ -0,0 +1,38 @@
+package lazyfs
+
+import (
+	"fmt"
+	"net/url"
+	"regexp"
+)
+
+func sourceFromPath(path string) (FileSource, error) {
+
+	// TODO.  Additional validatation of path
+	if len(path) == 0 {
+		return nil, fmt.Errorf("Zero length filename: %s", path)
+	}
+
+	var file FileSource
+	var err error
+
+	match, _ := regexp.MatchString("^http", path)
+	if match {
+		uri, err := url.Parse(path)
+		file, err = OpenHttpSource(*uri)
+		if err != nil {
+			return nil, fmt.Errorf("Error opening URL: %s", err.Error())
+		}
+	} else {
+		file, err = OpenLocalFile(path)
+		if err != nil {
+			return nil, fmt.Errorf("Error opening file: %s", err.Error())
+		}
+	}
+
+	if file == nil {
+		return nil, fmt.Errorf("Error creating file")
+	}
+
+	return file, nil
+}
